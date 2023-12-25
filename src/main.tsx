195e69b72f5a7ea840032b08f07c38e4b2312752
Alex Barbero <barrufat@hotmail.com>
2023-12-25 18:06:58 +0100

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <div>
+      <img src="./barru-logo.svg" alt="barru-logo" width="200" />
+    </div>
+  </React.StrictMode>
+);
