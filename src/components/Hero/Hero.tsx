cba1eabbb4aabd6757f3fc5509c6cb138a1b4c7b
Alex Barbero <barrufat@hotmail.com>
2023-12-27 00:04:29 +0100

    Add Hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,20 +1,31 @@
+import { useTranslation } from "react-i18next";
 import Reveal from "../Reveal/Reveal";
-import Gadjet from "../Gadjet/Gadjet";
+import HeroStyled from "./HeroStyled";
 
 const Hero = () => {
+  const [t, i18n] = useTranslation("global");
+
   return (
-    <>
-      <Gadjet />
-      <Reveal>
-        <h1>Lorem ipsum dolor sit amet.</h1>
-      </Reveal>
-      <Reveal>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam modi
-          quis tempora tempore ducimus reiciendis voluptas, nisi rem rerum eos.
-        </p>
-      </Reveal>
-    </>
+    <HeroStyled>
+      <div className="hero__wrapper">
+        <Reveal>
+          <span className="hero__secondary-title">
+            Hi there <span className="hero__detail">!</span>
+          </span>
+        </Reveal>
+        <Reveal>
+          <h1 className="hero__main-title">I'm Alex</h1>
+        </Reveal>
+        <Reveal>
+          <span className="hero__secondary-title">
+            <span className="hero__detail">FullStack</span> developer
+          </span>
+        </Reveal>
+        <Reveal>
+          <p className="hero__greeting-text">{t("presentacion1")}</p>
+        </Reveal>
+      </div>
+    </HeroStyled>
   );
 };
 
